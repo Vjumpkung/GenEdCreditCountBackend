305cc5e87420d6fed70355b9bb6c1d8ba44b5439
VjumpKunG <vjumpkung@gmail.com>
2023-11-29 21:14:37 +0700

    removed unwanted comment

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import router.login.login_controller as login
 import router.gened.gened_controller as gened
 
-app = FastAPI()
+app = FastAPI(title="Gened API", description="API for Gened", version="1.0.0")
 app.include_router(login.router)
 app.include_router(gened.router)
 
