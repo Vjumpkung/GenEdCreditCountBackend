473a162f6664ef3c9d9bf49f6eccf64ef210c1c4
VjumpKunG <vjumpkung@gmail.com>
2023-12-23 03:08:56 +0700

    remove LoginResponseDto because type is unclear.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import router.login.login_controller as login
 import router.gened.gened_controller as gened
@@ -7,11 +7,9 @@ app = FastAPI(title="Gened API", description="API for Gened", version="1.0.0")
 app.include_router(login.router)
 app.include_router(gened.router)
 
-origins = ["*"]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
--- router/login/login_controller.py
+++ router/login/login_controller.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter
 from router.login.LoginBodyDto import LoginBodyDto
 from router.login.login_service import loginService
-from router.login.LoginResponseDto import LoginResponseDto
 
 router = APIRouter(prefix="/login", tags=["login"])
 
 
 @router.post("/")
-async def login(loginBodyDto: LoginBodyDto) -> LoginResponseDto:
+async def login(loginBodyDto: LoginBodyDto):
     return await loginService(loginBodyDto)
