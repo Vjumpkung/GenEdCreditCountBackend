26e2418c5f504cd6b5ddda39b38b22227ee3e7c4
VjumpKunG <vjumpkung@gmail.com>
2023-12-29 14:13:55 +0700

    add prefix

--- main.py
+++ main.py
@@ -3,9 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 import router.login.login_controller as login
 import router.gened.gened_controller as gened
 
-app = FastAPI(title="Gened API", description="API for Gened", version="1.0.0")
-app.include_router(login.router)
-app.include_router(gened.router)
+app = FastAPI(
+    title="Gened API",
+    description="API for Gened",
+    version="1.0.0",
+)
+
+api_preix = "/api/v1"
+
+app.include_router(login.router, prefix=api_preix)
+app.include_router(gened.router, prefix=api_preix)
 
 app.add_middleware(
     CORSMiddleware,
